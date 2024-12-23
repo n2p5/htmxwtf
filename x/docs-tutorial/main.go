ddb23164b6b314480ea6f34bce11eb38d9f1c1ae
Nathan Toups <n@rjrbt.io>
2024-12-22 20:55:19 -0700

    wip: playing around with hx-triggers

--- x/docs-tutorial/main.go
+++ x/docs-tutorial/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 
@@ -12,6 +15,17 @@ func main() {
 
 	http.HandleFunc("/clicked", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("<p>woot<p>"))
+	})
+
+	counter := 0
+
+	http.HandleFunc("/mouse_entered", func(w http.ResponseWriter, r *http.Request) {
+		if counter >= 10 {
+			w.WriteHeader(286)
+		} else {
+			counter++
+		}
+		w.Write([]byte(fmt.Sprintf("<p>Mouse entered %d times</p>", counter)))
 	})
 
 	http.ListenAndServe(":8080", nil)

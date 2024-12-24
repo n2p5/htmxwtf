f05f6700af4c49211ca34a8baa2d71016b167b23
Nathan Toups <n@rjrbt.io>
2024-12-23 21:27:43 -0700

    feat: added tailwindcss, and dark mode

--- x/docs-tutorial/main.go
+++ x/docs-tutorial/main.go
@@ -5,6 +5,18 @@ import (
 	"net/http"
 )
 
+func mouseEnteredHandler() http.HandlerFunc {
+	counter := 0
+	return func(w http.ResponseWriter, r *http.Request) {
+		if counter >= 10 {
+			w.WriteHeader(286)
+		} else {
+			counter++
+		}
+		w.Write([]byte(fmt.Sprintf("<p>Mouse entered %d times</p>", counter)))
+	}
+}
+
 func main() {
 
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
@@ -14,19 +26,10 @@ func main() {
 	http.Handle("/", http.FileServer(http.Dir("./")))
 
 	http.HandleFunc("/clicked", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<p>woot<p>"))
+		w.Write([]byte(`<div id="parent-div"><p>woot<p></div>`))
 	})
 
-	counter := 0
-
-	http.HandleFunc("/mouse_entered", func(w http.ResponseWriter, r *http.Request) {
-		if counter >= 10 {
-			w.WriteHeader(286)
-		} else {
-			counter++
-		}
-		w.Write([]byte(fmt.Sprintf("<p>Mouse entered %d times</p>", counter)))
-	})
+	http.HandleFunc("/mouse_entered", mouseEnteredHandler())
 
 	http.ListenAndServe(":8080", nil)
 }

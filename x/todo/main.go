7d59b9e58c4f1416de6aaab097be790806baf45e
Nathan Toups <n@rjrbt.io>
2024-12-31 22:05:24 -0700

    wip

--- x/todo/main.go
+++ x/todo/main.go
@@ -41,7 +41,28 @@ func main() {
 }
 
 func (h handlers) getTodos(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("getTodos"))
+	todos := []Todo{}
+	
+	h.db.View(func(txn *badger.Txn) error {
+		it := txn.NewIterator(badger.DefaultIteratorOptions)
+		defer it.Close()
+		prefix := []byte("todo")
+		for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
+			item := it.Item()
+			
+			// TODO rip all this out into a separate function so that 
+			// the abstraction is cleaner in a speration of concerns
+			// I want to be able to interact with the DB in a way that
+			// is more abstracted from the HTTP handlers
+			err := item.Value(func(v []byte) error {
+				// TODO unmarshal v into a Todo struct
+				return nil
+			})
+				
+
+		return nil
+	})
+
 }
 
 func (h handlers) getTodo(w http.ResponseWriter, r *http.Request) {
@@ -58,4 +79,10 @@ func (h handlers) updateTodo(w http.ResponseWriter, r *http.Request) {
 
 func (h handlers) deleteTodo(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("deleteTodo"))
+}
+
+type Todo struct {
+	ID          string
+	Description string
+	Done        bool
 }
